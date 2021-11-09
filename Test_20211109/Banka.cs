b602970f5e324c674d0a8c98d03688c867ae32b0
Daniel Bohatec <91957021+dadasus2@users.noreply.github.com>
2021-11-09 15:22:16 +0100

    Add files via upload

---
+++ Test_20211109/Banka.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_20211109
+{
+    public class Banka
+    {
+        public string jmeno;
+        public float aktualniZustatek;
+        public string cisloUctu;
+        public float kontokorentLimit = -200;
+        public bool kontokorent;
+        public int max = 200000;
+        public int tydenniLimit = 10000;
+        public int platebniLimit = 10000;
+
+
+        public Banka(string jmeno, float aktualniZustatek, string cisloUctu)
+        {
+            this.jmeno = jmeno;
+            this.aktualniZustatek = aktualniZustatek;
+            this.cisloUctu = cisloUctu;
+        }
+        public void Vklad(float vklad)
+        {
+            if (vklad + aktualniZustatek <= max)
+            {
+                aktualniZustatek += vklad;
+                MessageBox.Show("Bylo vloženo: " + vklad.ToString());
+            }
+            else if (vklad + aktualniZustatek >= max)
+            {
+                MessageBox.Show("Nepodařilo se vložit z důvodu překročení limitu");
+                MessageBox.Show("Váš maximální limit je: " + max);
+            }
+        }
+        public void Vypis()
+        {
+            MessageBox.Show("Číslo účtu: " + cisloUctu + Environment.NewLine + "Aktuální zůstatek: " + aktualniZustatek + Environment.NewLine + "Týdenní limit: " + tydenniLimit + Environment.NewLine + "Platební limit: " + platebniLimit);
+        }
+
+        public void Vyber(float vyber)
+        {
+
+            if (vyber - aktualniZustatek <= 0 && vyber <= tydenniLimit)
+            {
+                aktualniZustatek -= vyber;
+                MessageBox.Show("vybrali ste: " + vyber.ToString());
+            }
+            else if (vyber - aktualniZustatek > 0 && kontokorent == false)
+            {
+                MessageBox.Show("ses bezdák");
+            }
+            else if (vyber - aktualniZustatek > kontokorentLimit && kontokorent == true)
+            {
+                aktualniZustatek -= vyber;
+                MessageBox.Show("Bylo vybráno: " + vyber.ToString());
+                if (aktualniZustatek <= kontokorentLimit)
+                {
+                    MessageBox.Show("Byl překročen imit");
+                    aktualniZustatek = kontokorentLimit;
+                }
+            } 
+            else
+            {
+                MessageBox.Show("Překročili jste týdenní limit!!");
+            }
+        }
+        public void Navyseni(int navyseni)
+        {
+            if (navyseni > tydenniLimit)
+            {
+                tydenniLimit = navyseni;
+                MessageBox.Show("Navýšili jste svůj výběr na: " + tydenniLimit);
+            }
+            if (navyseni < tydenniLimit)
+            {
+                tydenniLimit = navyseni;
+                MessageBox.Show("Zmenšili jste svůj výber na: " + tydenniLimit);
+            }
+        }
+        public void NavyseniPlatby(int navyseni)
+        {
+            if (navyseni > platebniLimit)
+            {
+                platebniLimit = navyseni;
+                MessageBox.Show("Navýšili jste svůj platební limit na: " + platebniLimit);
+            }
+            if (navyseni < platebniLimit)
+            {
+                platebniLimit = navyseni;
+                MessageBox.Show("Zmenšili jste svůj platební limit na: " + platebniLimit);
+            }
+        }
+        public void Platba (float platba, string cisloUctu)
+        {
+            if (platba - aktualniZustatek <= 0 && platba <= platebniLimit)
+            {
+                aktualniZustatek -= platba;
+                MessageBox.Show("Transakce byla provedena na číslo učtu: " + cisloUctu + " s částkou:" + platba);
+                MessageBox.Show("Aktualní zůstatek na účtě: " + aktualniZustatek);
+            }
+            else if (platba - aktualniZustatek > 0)
+            {
+                MessageBox.Show("Nemáte dostatek peněz na provedení transakce");
+            }
+            else
+            {
+                MessageBox.Show("Nepodařilo se akci dokončit, došlo k překročení platebního limitu");
+            }
+        }
+    }
+}
---
+++ Test_20211109/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_20211109
+{
+    public partial class Form1 : Form
+    {
+        Banka banka;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            banka = new Banka("Daniel Bohatec", 2000, "54515/411" );
+            labelJmeno.Text = banka.jmeno;
+            labelCisloUctu.Text = banka.cisloUctu.ToString();
+            labelAktualni.Text = banka.aktualniZustatek.ToString();
+
+            groupBox2.Visible = false;
+            buttonZmena2.Visible = false;
+        }
+
+        private void buttonVlozit_Click(object sender, EventArgs e)
+        {
+            float vklad = float.Parse(textBoxHodnota.Text);
+            banka.Vklad(vklad);
+            labelAktualni.Text = banka.aktualniZustatek.ToString();
+        }
+
+        private void buttonVybrat_Click(object sender, EventArgs e)
+        {
+            float vyber = float.Parse(textBoxHodnota.Text);
+            banka.Vyber(vyber);
+            labelAktualni.Text = banka.aktualniZustatek.ToString();
+        }
+
+        private void buttonZmena_Click(object sender, EventArgs e)
+        {
+            groupBox1.Visible = false;
+            groupBox2.Visible = true;
+            buttonZmena.Visible = false;
+            buttonZmena2.Visible = true;
+        }
+
+        private void buttonZmenaTyden_Click(object sender, EventArgs e)
+        {
+            int navyseni = int.Parse(textBoxTydenniLimit.Text);
+            banka.Navyseni(navyseni);
+        }
+
+        private void buttonZmena2_Click(object sender, EventArgs e)
+        {
+            groupBox1.Visible = true;
+            groupBox2.Visible = false;
+            buttonZmena.Visible = true;
+            buttonZmena2.Visible = false;
+        }
+
+        private void buttonVypis_Click(object sender, EventArgs e)
+        {
+            banka.Vypis();
+        }
+
+        private void buttonPlatit_Click(object sender, EventArgs e)
+        {
+            float platba = float.Parse(textBoxPlatba.Text);
+            string cisloUctu = textBoxPlatbaCisloUctu.Text;
+            banka.Platba(platba,cisloUctu);
+            labelAktualni.Text = banka.aktualniZustatek.ToString();
+        }
+
+        private void buttonZmenaPlatba_Click(object sender, EventArgs e)
+        {
+            int navyseni = int.Parse(textBoxPlatebniLimit.Text);
+            banka.NavyseniPlatby(navyseni);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                banka.kontokorent = true;
+            }
+            if (checkBox1.Checked == false)
+            {
+                banka.kontokorent = false;
+            }
+        }
+    }
+}
